14a60cbb44f6b0d40d13e10da941a5ab6959a8ae
mrbama1010 <markvandenhurk@gmail.com>
2016-12-09 11:04:24 +0100

    Added GetFeedbacklist and Details, TO DO del button

--- SMProofOfConcept/GetFeedback.Designer.cs
+++ SMProofOfConcept/GetFeedback.Designer.cs
@@ -28,34 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lbx_Feedback = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_From = new System.Windows.Forms.Label();
+            this.lb_Date = new System.Windows.Forms.Label();
+            this.lb_Category = new System.Windows.Forms.Label();
+            this.tb_Details = new System.Windows.Forms.TextBox();
+            this.btn_Verwijder = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lbx_Feedback
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(0, -1);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(281, 654);
-            this.listBox1.TabIndex = 0;
+            this.lbx_Feedback.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbx_Feedback.FormattingEnabled = true;
+            this.lbx_Feedback.ItemHeight = 16;
+            this.lbx_Feedback.Location = new System.Drawing.Point(0, -1);
+            this.lbx_Feedback.Name = "lbx_Feedback";
+            this.lbx_Feedback.Size = new System.Drawing.Size(281, 644);
+            this.lbx_Feedback.TabIndex = 0;
+            this.lbx_Feedback.SelectedIndexChanged += new System.EventHandler(this.lbx_Feedback_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(298, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "From:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(298, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Category:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(298, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Date:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(298, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Details:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lb_From
+            // 
+            this.lb_From.AutoSize = true;
+            this.lb_From.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_From.Location = new System.Drawing.Point(466, 20);
+            this.lb_From.Name = "lb_From";
+            this.lb_From.Size = new System.Drawing.Size(85, 31);
+            this.lb_From.TabIndex = 5;
+            this.lb_From.Text = "From:";
+            this.lb_From.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lb_Date
+            // 
+            this.lb_Date.AutoSize = true;
+            this.lb_Date.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Date.Location = new System.Drawing.Point(466, 60);
+            this.lb_Date.Name = "lb_Date";
+            this.lb_Date.Size = new System.Drawing.Size(85, 31);
+            this.lb_Date.TabIndex = 6;
+            this.lb_Date.Text = "From:";
+            this.lb_Date.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lb_Category
+            // 
+            this.lb_Category.AutoSize = true;
+            this.lb_Category.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Category.Location = new System.Drawing.Point(466, 100);
+            this.lb_Category.Name = "lb_Category";
+            this.lb_Category.Size = new System.Drawing.Size(85, 31);
+            this.lb_Category.TabIndex = 7;
+            this.lb_Category.Text = "From:";
+            this.lb_Category.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tb_Details
+            // 
+            this.tb_Details.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Details.Location = new System.Drawing.Point(304, 180);
+            this.tb_Details.Multiline = true;
+            this.tb_Details.Name = "tb_Details";
+            this.tb_Details.Size = new System.Drawing.Size(686, 463);
+            this.tb_Details.TabIndex = 8;
+            // 
+            // btn_Verwijder
+            // 
+            this.btn_Verwijder.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Verwijder.Location = new System.Drawing.Point(801, 20);
+            this.btn_Verwijder.Name = "btn_Verwijder";
+            this.btn_Verwijder.Size = new System.Drawing.Size(189, 151);
+            this.btn_Verwijder.TabIndex = 9;
+            this.btn_Verwijder.Text = "Verwijder";
+            this.btn_Verwijder.UseVisualStyleBackColor = true;
+            this.btn_Verwijder.Click += new System.EventHandler(this.btn_Verwijder_Click);
             // 
             // GetFeedback
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1002, 653);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(1000, 643);
+            this.Controls.Add(this.btn_Verwijder);
+            this.Controls.Add(this.tb_Details);
+            this.Controls.Add(this.lb_Category);
+            this.Controls.Add(this.lb_Date);
+            this.Controls.Add(this.lb_From);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbx_Feedback);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "GetFeedback";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GetFeedback";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lbx_Feedback;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_From;
+        private System.Windows.Forms.Label lb_Date;
+        private System.Windows.Forms.Label lb_Category;
+        private System.Windows.Forms.TextBox tb_Details;
+        private System.Windows.Forms.Button btn_Verwijder;
     }
 }
--- SMProofOfConcept/GetFeedback.cs
+++ SMProofOfConcept/GetFeedback.cs
@@ -7,14 +7,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SMProofOfConcept.Classes.Database;
+using Newtonsoft.Json;
 
 namespace SMProofOfConcept
 {
     public partial class GetFeedback : Form
     {
+        private DatabaseLogin login;
         public GetFeedback(DatabaseLogin login)
         {
             InitializeComponent();
+            this.login = login;
+            lb_From.Text = "";
+            lb_Date.Text = "";
+            lb_Category.Text = "";
+
+            GetFeedbackFromDatabase();
+
+        }
+
+        private void GetFeedbackFromDatabase()
+        {
+            DatabaseConnection dbCon = new DatabaseConnection();
+            string query = "SELECT * FROM SMFeedback WHERE SendTo = '";
+            query += login.Username + "'";
+
+            string jsonString = dbCon.sendQuery(query);
+            DatabaseFeedback[] dbFeedback = JsonConvert.DeserializeObject<DatabaseFeedback[]>(jsonString);
+
+            UpdateListBox(dbFeedback);
+        }
+
+        private void UpdateListBox(DatabaseFeedback[] dbFeedback)
+        {
+            lbx_Feedback.Items.Clear();
+            List<DatabaseFeedback> feedbackList = dbFeedback.ToList();
+
+            feedbackList.Sort((x, y) => x.ToString().CompareTo(y.ToString()));
+
+            for (int i = feedbackList.Count; i>0; i--)
+            {
+                lbx_Feedback.Items.Add(feedbackList[i - 1]);
+            }
+
+        }
+
+        private void lbx_Feedback_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DatabaseFeedback dbFeedback = (DatabaseFeedback)lbx_Feedback.SelectedItem;
+            lb_From.Text = dbFeedback.SendFrom;
+            lb_Date.Text = dbFeedback.DateTime;
+            lb_Category.Text = dbFeedback.Category;
+            tb_Details.Text = dbFeedback.Feedback;
+        }
+
+        private void btn_Verwijder_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class DatabaseFeedback
+    {
+        public string SendTo { get; set; }
+        public string SendFrom { get; set; }
+        public string Feedback { get; set; }
+        public string Category { get; set; }
+        public string DateTime { get; set; }
+
+        public override string ToString()
+        {
+            return DateTime + ", " + SendFrom + ", " + Category;
         }
     }
 }
